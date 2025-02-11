dfb19a69fbd3074769c49be66920caa09198380e
Abu Bakar Syed <sdzubair.engg@gmail.com>
2025-02-10 21:33:05 -0600

    updated comments

--- src/helpers/rest_api_helpers.ts
+++ src/helpers/rest_api_helpers.ts
@@ -2,7 +2,13 @@ import { env } from '../../environments/environment';
 import addContext from 'mochawesome/addContext';
 import moment, { Moment } from 'moment';
 
-
+/*
+getApi: this function is designed to get the rest API response using GET method
+@param: _this: Mocha reporting context
+@param: application: the framework designed to support multiple application, provide the application name to get the environment details
+@param: uri: provide the uri, this will be concated with baseUri
+@param: environment: environment to run the script
+*/
 export const getApi = async (_this: Mocha.Context, application: string | number, uri: any, environment: any) => {
   try {
     const env1: string = environment;
@@ -32,6 +38,23 @@ export const getApi = async (_this: Mocha.Context, application: string | number,
   
 }
 
+
+/*
+postApi: this function is designed to post a rest API request and get the response using POST method
+@param: _this: Mocha reporting context
+@param: application: the framework designed to support multiple application, provide the application name to get the environment details
+@param: uri: provide the uri, this will be concated with baseUri
+@param: environment: environment to run the script
+@param: request: provide the request body including headers if needed
+        example request = {
+          headers: {
+            'content-type': 'application/json
+          },
+          body: {
+            'name': 'test'
+          }
+        }
+*/
 export const postApi = async (_this: Mocha.Context, application: string, uri: any, environment: string = 'sit1', request : any) => {
     
   const env1: string = environment;
@@ -76,6 +99,15 @@ export const postApi = async (_this: Mocha.Context, application: string, uri: an
 }
 
 
+/*
+apiReport: this function is designed to report all the necessary details
+@param: _this: Mocha reporting context
+@param: startTime: this is unecessary to get the information on when the API was hit
+@param: responseTime: this will provide the information on how much time API is taking to get the response
+@param: endpoint: endpoint of an API
+@param: request: provides the detail of request body
+@param: response: response of an API
+*/
 export const apiReport = async (_this: Mocha.Context, startTime: Moment, responseTime: number, endpoint: string, request: any, response: JSON) => {
   
   addContext(_this, {
